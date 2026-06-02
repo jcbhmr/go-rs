003d9320275c527b4eb7dccb9d1df68da2f7e311
Jacob Hummer <jcbhmr@outlook.com>
2026-06-02 04:51:11 +0000

    works on linux amd64

--- src/bin/go.rs
+++ src/bin/go.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error, io::Cursor, process::Command};
+use cross_exec::CommandExt;
+
+static ZIP: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/go.zip"));
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let current_exe = env::current_exe()?;
+    let data_dir = current_exe.with_added_extension("data");
+    if !data_dir.try_exists()? {
+        fs_err::create_dir_all(&data_dir)?;
+        let mut archive = zip::ZipArchive::new(Cursor::new(ZIP))?;
+        archive.extract(&data_dir)?;
+    }
+    Err(Command::new(data_dir.join("bin").join(format!("go{}", if cfg!(windows) { ".exe" } else { "" })))
+        .args(env::args_os().skip(1))
+        .cross_exec().into())
 }
